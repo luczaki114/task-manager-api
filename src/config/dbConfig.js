5b683903f581ac0eb96179400c2595588f802c00
Ireneusz (Eric) Luczak <luczak.irek@gmail.com>
2025-02-20 12:12:19 -0600

    throwing error when mongo uri is not defined

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined in environment variables');
+        }
+        
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected successfully');
         
